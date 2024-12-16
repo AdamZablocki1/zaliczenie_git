d1f07c532aea8d1562b4fda18605620b6959377f
AdamZablocki1 <251071@edu.p.lodz.pl>
2024-12-16 09:08:32 +0100

    1 zmiana (#1)
    * 1 zmiana
    
    * 2 zmiana

--- main.cpp
+++ main.cpp
@@ -2,5 +2,14 @@
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    int cyfra;
+    printf("Zmiana numer 1\nPodaj liczbe\n");
+    scanf("%d", &cyfra);
+    printf("oto twoja liczba: %d\n", cyfra);
+    printf("Zmiana numer 2\n");
+    printf("odliczanie do liczby 9\n");
+    for (int i = 0; i < 9; i++) {
+        printf("%d ", i);
+    }
     return 0;
 }
