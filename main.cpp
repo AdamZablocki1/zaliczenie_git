ff37590dfef031ef727c5549e3763402e2f7a6c4
student <251071@edu.p.lodz.pl>
2024-12-16 08:58:58 +0100

    1 zmiana

--- main.cpp
+++ main.cpp
@@ -2,5 +2,9 @@
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    int cyfra;
+    printf("Zmiana numer 1\nPodaj liczbe\n");
+    scanf("%d", &cyfra);
+    printf("oto twoja liczba: %d", cyfra);
     return 0;
 }
